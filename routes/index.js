f7908dc3057864b52cd1db23794bb4592b8f3df0
maozeyu <5400230+maozeyu@user.noreply.gitee.com>
2019-11-27 17:38:47 +0800

    new

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const validUrl = require('valid-url');
+const shortId = require('shortid');
+const config = require('config');
+
+
+module.exports = router;
